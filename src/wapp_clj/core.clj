219da4d48c05bb6b68c6af3ac63f2c3945cea096
Binay Mishra <binaymishramca@gmail.com>
2023-03-07 19:01:05 +0530

    Initial commit

---
+++ src/wapp_clj/core.clj
@@ -0,0 +1,17 @@
+(ns wapp-clj.core
+  (:require [compojure.core :refer [defroutes GET]]
+            [ring.adapter.jetty :as ring])
+  (:gen-class))
+
+(defn binay-to [first second]
+  apply (str first second))
+
+
+(defroutes routes
+  (GET "/" []
+    (reduce binay-to ["<h2>Hello World</h2>"
+                      "reduce binay-to"
+                      " Maverick" ", Loner" ", Engineer"])))
+
+(defn -main []
+  (ring/run-jetty #'routes {:port 9000 :join? false}))
